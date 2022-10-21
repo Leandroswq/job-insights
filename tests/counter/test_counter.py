e54415a1acb35ba36452c84f35119f1d72a77107
Leandroswq <leandrobonfim15@hotmail.com>
2022-10-21 08:47:23 -0300

    test: Testa a contagem de palavras em um documento

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,12 @@
 # from src.counter import count_ocurrences
 
 
+from src.counter import count_ocurrences
+
+
 def test_counter():
-    pass
+    path = "src/jobs.csv"
+    count = count_ocurrences(path, "test")
+
+    excepted = 2687
+    assert count == excepted
