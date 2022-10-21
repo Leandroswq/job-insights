b295480d934d77c6cccf6ce051aa0d7a24f848e5
Leandroswq <leandrobonfim15@hotmail.com>
2022-10-21 09:29:51 -0300

    test: Testa a tradução das chaves do jobo de pt-br para en

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,19 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    path = "tests/mocks/brazilians_jobs.csv"
+    jobs = read_brazilian_file(path)
+    job_keys = [key for key in jobs[0].keys()]
+    job_keys.sort()
+    
+    valid_keys = ["title", "salary", "type"]
+    valid_keys.sort()
+    
+    is_valid_array = [
+        False
+        for key_index in range(len(valid_keys))
+        if valid_keys[key_index] != job_keys[key_index]
+    ]
+
+    assert len(is_valid_array) == 0
